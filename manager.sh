4c2d612dec4d644c21f527af29fe3f2c07e7d172
Samir Seamari <samirsiemari@gmail.com>
2026-04-17 15:59:09 +0200

    third commit

--- manager.sh
+++ manager.sh
@@ -7,4 +7,32 @@ echo "3 Search student"
 echo "4 Exit"
 echo "---------------"
 read num
+if [ $num = 1 ]; then
+        echo "Add student"
+        echo "Name"
+        read name
+        echo "Age"
+        read age
+        if [ $age -lt 18 ]; then
+                echo "Student is minor"
+        else
+                {
+                        echo "Name: $name - $age Years old"
+                } >> students.txt
+        echo "Save"
+        fi
+elif [ "$num" -eq 2 ]; then
+        cat students.txt
+elif [ "$num" -eq 3 ]; then
+        echo "Student name"
+        read student
+        if #true; then
+                grep -n $student students.txt; then
+                echo "Found"
+        else
+                        echo "Not found"
+        fi
+elif [ "$num" -eq 4 ]; then
+        break
+fi
 done
